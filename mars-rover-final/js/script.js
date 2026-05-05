c115ddcc4d8ab25ce99bce3b991ca9d928a2076f
Adyncunningham <alcunningha2@ualr.edu>
2026-05-05 04:28:09 +0000

    css changes

---
+++ mars-rover-final/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('.fade').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        slide: 'post',
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+
+
+});
